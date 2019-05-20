4e35aaaf56f78ac0928ea1b59f76ab4b726a47c3
Ingrid Bazin <ingrid.bazin31@gmail.com>
2019-05-20 16:56:35 +0200

    Add DatePickers

--- Onvaou/ViewController.swift
+++ Onvaou/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var departure_text_field: UITextField!
+    @IBOutlet weak var departure_display_field: UITextView!
+    @IBOutlet weak var way_date_picker: UIDatePicker!
+    @IBOutlet weak var return_date_picker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func setDepartureAirport(_ sender: Any) {
+        let departure = departure_text_field.text
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd MMMM YYYY"
+        let wayDate = dateFormatter.string(from: way_date_picker.date)
+        let returnDate = dateFormatter.string(from: return_date_picker.date)
+        if let depText = departure {
+            let textToBeAdded: String = "From \(depText) \(wayDate) to \(returnDate)\n"
+        departure_display_field.insertText(textToBeAdded)
+        }
+        
+        
+        
+    }
+
+    
 
 }
 
