727425349593ef3c756e732e579430f88e2e21bb
Ingrid Bazin <ingrid.bazin31@gmail.com>
2019-05-27 17:02:29 +0200

    Add pax field

--- Onvaou/ViewController.swift
+++ Onvaou/ViewController.swift
@@ -8,12 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    
+    var dataSource = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9"]
     @IBOutlet weak var departure_text_field: UITextField!
     @IBOutlet weak var display_field: UITextView!
     @IBOutlet weak var way_date_picker: UIDatePicker!
     @IBOutlet weak var return_date_picker: UIDatePicker!
+    @IBOutlet weak var adultsPicker: UIPickerView!
+    @IBOutlet weak var childrenPickerr: UIPickerView!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +26,15 @@ class ViewController: UIViewController, UITextFieldDelegate {
         departure_text_field.becomeFirstResponder()
         departure_text_field.delegate = self
         departure_text_field.returnKeyType = UIReturnKeyType.search
+        self.adultsPicker.dataSource = self
+        self.adultsPicker.delegate = self
+        self.childrenPickerr.dataSource = self
+        self.childrenPickerr.delegate = self
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        onSearchClicked(textField)
+        return true
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -39,22 +53,42 @@ class ViewController: UIViewController, UITextFieldDelegate {
             self.view.backgroundColor = UIColor.white
         }
     }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return dataSource.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView,
+                    titleForRow row: Int,
+                    forComponent component: Int) -> String? {
+        
+        // Return a string from the array for this row.
+        return dataSource[row]
+    }
 
-
-    @IBAction func onOkClicked(_ sender: Any) {
+    @IBAction func onSearchClicked(_ sender: Any) {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd MMMM YYYY"
+        dateFormatter.locale = Locale(identifier: "fr_FR")
         let date1 = way_date_picker.date
         let date2 = return_date_picker.date
         if let depText = departure_text_field.text {
-            let textToBeAdded: String = "From \(depText) \(dateFormatter.string(from: date1)) to \(dateFormatter.string(from: date2))\n"
-            let existingText = UserData.displayViewText.replacingOccurrences(of: UserData.DEFAULT_TEXT, with: "")
-            display_field.text = existingText + textToBeAdded
+            let textToDisplay: String = "Nous recherchons des vols au départ de \(depText), du \(dateFormatter.string(from: date1)) au \(dateFormatter.string(from: date2))"
+            UserData.displayViewText = textToDisplay
+            display_field.text = textToDisplay
         }
         UserData.displayViewText = display_field.text
+        if checkDates(date1: date1, date2: date2) && checkDeparture(city: departure_text_field.text) {
+            
+        }
         departure_text_field.text = nil
         departure_text_field.resignFirstResponder()
-        checkDates(date1: date1, date2: date2)
+        
     }
     
     @IBAction func onResetClicked(_ sender: Any) {
@@ -62,11 +96,26 @@ class ViewController: UIViewController, UITextFieldDelegate {
         display_field.text = UserData.displayViewText
     }
     
-    func checkDates(date1: Date, date2: Date){
+    func checkDates(date1: Date, date2: Date) -> Bool{
         if date2 < Date() {
-            print("the trip is planned in the past")
+            display_field.text.append("the trip is planned in the past")
+            return false
         } else if date1 > date2 {
-            print("the return date is set before the departure")
+            display_field.text.append("the return date is set before the departure")
+            return false
+        }
+        else {
+            return true
+        }
+    }
+    
+    func checkDeparture(city: String?) -> Bool{
+        if city == "" || city == nil{
+            display_field.text.append("\nPlease select a city")
+            return false
+        }
+        else {
+            return true
         }
     }
 
